c1cc804241df81e324ca4d206788db3ae5f234b5
Hasibul-Islam-Shanto <mdhasibulislamshanto007@gmail.com>
2024-05-14 15:26:42 +0600

    Add Suspance for useSearchParams

--- src/components/headItem.tsx
+++ src/components/headItem.tsx
@@ -1,7 +1,6 @@
 "use client";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
-import React, { Suspense } from "react";
 type PropsType = {
   title: string;
   path: string;
@@ -10,17 +9,15 @@ const HeadItem = ({ title, path }: PropsType) => {
   const searchParam = useSearchParams();
   const genre = searchParam.get("genre");
   return (
-    <Suspense fallback={<div>Loading....</div>}>
-      <Link
-        href={`/?genre=${path}`}
-        className={`text-lg font-semibold ${
-          genre === path &&
-          "underline underline-offset-8 decoration-amber-500 decoration-4"
-        }`}
-      >
-        {title}
-      </Link>
-    </Suspense>
+    <Link
+      href={`/?genre=${path}`}
+      className={`text-lg font-semibold ${
+        genre === path &&
+        "underline underline-offset-8 decoration-amber-500 decoration-4"
+      }`}
+    >
+      {title}
+    </Link>
   );
 };
 
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import HeadItem from "./headItem";
-
+import { Suspense } from "react";
 const Header = () => {
   return (
-    <div className="flex justify-center items-center gap-x-5 bg-amber-100 dark:bg-gray-600 p-4 mt-5">
-      <HeadItem title="Trending Movies" path="fetchTrending" />
-      <HeadItem title="Top Rate Movies" path="fetchTopRated" />
-    </div>
+    <Suspense fallback={<span>Loading...</span>}>
+      <div className="flex justify-center items-center gap-x-5 bg-amber-100 dark:bg-gray-600 p-4 mt-5">
+        <HeadItem title="Trending Movies" path="fetchTrending" />
+        <HeadItem title="Top Rate Movies" path="fetchTopRated" />
+      </div>
+    </Suspense>
   );
 };
 
